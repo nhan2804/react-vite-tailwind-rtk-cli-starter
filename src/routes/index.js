729a7ca2dadc8e02106939fb29afb403416948ac
nhan2804 <nnnhan2804@gmail.com>
2024-06-19 13:18:16 +0700

    h

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
 import authRoute from "@modules/auth/routes";
 import projectsRoutes from "@modules/projects/routes";
-import storeProjectRoutes from "@modules/store-projects/routes";
 const routes = [
   ///////////////////////////////for cli insert
   ...authRoute,
   ...projectsRoutes,
-  ...storeProjectRoutes,
 ];
 export default routes;
